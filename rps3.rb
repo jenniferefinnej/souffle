0aab61580a51a1f5b43a1cdcc931e943682b089e
jenniferefinnej <j3nniferefinn3j@gmail.com>
2016-01-26 21:56:23 -0600

    Create rps3.rb

---
+++ rps3.rb
@@ -0,0 +1,62 @@
+wins = 0
+loses = 0
+#Create a variable counter for wins, set to zero
+#Create a variable counter for loses, set to zero
+
+
+#Iterate Three times do 
+# ^ rounds
+	#Display, chose Rock, Paper or Scissors
+	puts "Chose - rock, paper, or scissors?"
+	chose = gets.chomp
+	#Get user input, set to variable choice
+	
+	#Get random selection from an array, set to variable comp
+
+	#Start an if statement: if both choices are the same
+if both choices the same
+	then puts draw
+elsif computer has rock and human has paper
+	add point for win
+elsif computer has rock and human has scissors
+	add point for loses
+elsif computer has scissors and human has rock
+	add point for win
+elsif computer has scissors and human has paper
+	add point for loses
+elsif computer has paper and human has rock
+	add point for loses
+elsif computer has paper and human has scissors
+	add point for win
+end 
+
+if wins > loses 
+	display "You win!"
+
+else wins < loses
+	display "You lose :("
+end
+
+		#Puts draw
+	#else if computer has rock and human has paper
+		#add a point for win
+	#else if computer has rock and human has scissors
+		#add a point for loses
+	#else if computer has scissors and human has rock
+		#add a point for win
+	#else if computer has scissors and human has paper
+		#add a point for loses
+	#else if computer has paper and human has rock
+		#add a point for loses
+	#else if computer has paper and human has scissors
+		#add a point for win
+
+
+#If wins > loses
+	#Display You win
+#Else
+	#Display You Lose
+
+
+#### BONUS #### 
+	#Display History of Moves at end
